2f7d03b5f05afba71e1087baf0af4e33b6070624
ishizukamaaya <maayajas@gmail.com>
2021-08-18 14:50:57 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :books
   resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
 
