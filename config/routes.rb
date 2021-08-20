51e314ea529c07ca15a03c382d4c266783589e3d
ishizukamaaya <maayajas@gmail.com>
2021-08-20 10:17:07 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+
+    resource :relationships, only: [:create, :destroy]
+    get 'follow' => 'relationships#follow', as: 'follow'
+    get 'follower' => 'relationships#follower', as: 'follower'
+  end
+
+
   resources :books do
     resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
