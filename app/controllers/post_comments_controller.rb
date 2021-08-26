e1d49e499b605ac70f55bf49d82be56d348bd1f2
ishizukamaaya <maayajas@gmail.com>
2021-08-26 13:29:31 +0000

    finish

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,19 +1,20 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @post_comment = PostComment.new(post_comment_params)
+    @post_comment.book_id = @book.id
+    @post_comment.user_id = current_user.id
+    unless @post_comment.save
+    end
   end
 
   def destroy
-    PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    post_comment = @book.post_comments.find(params[:id])
+    post_comment.destroy
   end
 
   private
-
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
