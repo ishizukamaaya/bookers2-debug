8e942cf01dffff8e0d4ab1a9be3f85539007afe0
ishizukamaaya <maayajas@gmail.com>
2021-09-05 06:04:24 +0000

    fihish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_121641) do
+ActiveRecord::Schema.define(version: 2021_09_05_012403) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_09_04_121641) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "evaluation"
   end
 
   create_table "entries", force: :cascade do |t|
